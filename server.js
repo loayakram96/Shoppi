1339d9c878b910f5a74be493a8727eba5fa78ec7
loayakram96 <loay.akram.2006@gmail.com>
2020-08-17 21:24:54 +0200

    Version 1.0.1

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const path = require()
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
@@ -27,6 +27,7 @@ app.use('/api/items', items);
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
+
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
